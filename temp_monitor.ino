44c499b669ab39b5e280ac8b56416f42d8a910be
scott <sdunifon@gmail.com>
2014-11-25 19:04:44 -0500

    ADDED: buzz added + flash added + temp sensor added

--- temp_monitor.ino
+++ temp_monitor.ino
@@ -1,29 +1,77 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#ifndef temperature_h
-#define temperature_h
+#include "temperature.h"
+// Data wire is plugged into pin 3 on the Arduino
+#define ONE_WIRE_BUS 3
+#define FLASH_PIN 5
 
-#include <WString.h>
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature tempSensors(&oneWire);
 
 
 
-typedef struct RGB {
-  int red;
-  int green;
-  int blue; 
-} rgb;
 
-typedef struct Thermometer {
-  String label;
-  DeviceAddress address;
-  float current_temp;
-  RGB temp_color;
-} thermometer;
 
-typedef struct LedRGB {
-  int red_pin;
-  int green_pin;
-  int blue_pin;
-} led_rgb;
 
-#endif
+
+DeviceAddress tempSensor1Address =  {0x28, 0xFF, 0x98, 0x55, 0x73, 0x04, 0x00, 0xA8};
+DeviceAddress tempSensor2Address = {0x28, 0xFF, 0xF6, 0x56, 0x77, 0x04, 0x00, 0x0D};
+
+
+DallasTemperature sensors(&oneWire);
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  // Start up the library
+  sensors.begin();
+  // set the resolution to 10 bit (good enough?)
+  sensors.setResolution(tempSensor1Address, 10);
+  sensors.setResolution(tempSensor2Address, 10);
+  
+  pinMode(FLASH_PIN, OUTPUT);     
+
+}
+
+void printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  if (tempC == -127.00) {
+    Serial.print("Error getting temperature");
+  } else {
+    Serial.print("C: ");
+    Serial.print(tempC);
+    Serial.print(" F: ");
+    Serial.print(DallasTemperature::toFahrenheit(tempC));
+  }
+}
+
+void loop(void)
+{ 
+  delay(250);
+  Serial.print("Getting temperatures...\n\r");
+  sensors.requestTemperatures();
+  
+  Serial.print("Inside temperature is: ");
+  printTemperature(tempSensor1Address);
+  Serial.print("\n\r");
+  Serial.print("Outside temperature is: ");
+  printTemperature(tempSensor2Address);
+  Serial.print("\n\r\n\r");
+  buzz();
+  flash();
+}
+
+void buzz(){
+  int buzz_duration = 500;
+  tone(6,1000,buzz_duration);
+  delay(buzz_duration);
+
+}
+
+void flash(){
+  digitalWrite(FLASH_PIN, HIGH);
+  delay(100);
+  digitalWrite(FLASH_PIN, LOW);
+}
